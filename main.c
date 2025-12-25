e32006584a14a7befd4a0d18b709d8bb705c06c1
Malik Adebayo <malik.adebayo@matlinux.com>
2025-12-25 21:15:32 +0000

    Commit of main file as first

---
+++ main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(){
+
+    printf("Hello, World!, time is %s\n\n", __TIME__);
+    printf("%s \n\n",__DATE__);
+    return 0;
+}
