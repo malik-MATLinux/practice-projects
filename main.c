7125f62f83e6ef53d0304c9c7ca7663d2f071d76
Malik Adebayo <malik.adebayo@matlinux.com>
2025-12-26 12:35:05 +0000

    commit today 26th Dec 2025

--- main.c
+++ main.c
@@ -4,7 +4,24 @@
 
 int main(){
 
+    char questions[][100] = {
+        "What is your name?",
+        "How old are you?",
+        "What is your favorite programming language?"
+    };
+
     printf("Hello, World!, time is %s\n\n", __TIME__);
     printf("%s \n\n",__DATE__);
+    printf("Number of questions is : %ld\n\n", sizeof(questions) / sizeof(questions[0]));
+
+
+    /*  Ternery operator below
+    */
+
+    int a = 10;
+    int b = 20;
+    int max;    
+
+    max = (a + b == 30) ? printf("a plus b is 30\n\n") : printf("wrong answer not 50\n\n");
     return 0;
 }
